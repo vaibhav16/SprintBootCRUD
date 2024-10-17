b5901a1dfac41d447195bb7e8671060decd51e60
Vaibhav Sharma <vssharma.vaibhav5@gmail.com>
2024-10-17 19:04:42 +0530

    initial commit

---
+++ java-workspace/taskmanager_springboot_crud2/src/main/java/com/example/taskmanager_springboot_crud2/TaskmanagerSpringbootCrud2Application.java
@@ -0,0 +1,13 @@
+package com.example.taskmanager_springboot_crud2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskmanagerSpringbootCrud2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskmanagerSpringbootCrud2Application.class, args);
+    }
+
+}
---
+++ java-workspace/taskmanager_springboot_crud2/src/test/java/com/example/taskmanager_springboot_crud2/TaskmanagerSpringbootCrud2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.taskmanager_springboot_crud2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskmanagerSpringbootCrud2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
