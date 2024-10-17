d5cb6434363d55115da436dce8f0149ea21e88d2
Vaibhav Sharma <vssharma.vaibhav5@gmail.com>
2024-10-17 18:56:03 +0530

    hello world

---
+++ java-workspace/taskmanager_springboot_crud2/src/main/java/com/example/taskmanager_springboot_crud2/HelloController.java
@@ -0,0 +1,12 @@
+package com.example.taskmanager_springboot_crud2;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    String hello(){
+        return "Hello World!";
+    }
+}
